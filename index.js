8da5903dd782a8197c5daea54f5e64568429bcf0
Arpitadey88 <deyarpitabanik@gmail.com>
2021-10-31 16:05:18 +0600

    added tourism server side

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqkcw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('tourPlan');
+        const servicesCollection = database.collection('services');
+        const orderCollection = database.collection('orders');
+        // console.log('connected to database');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // GET SINGLE SERVICE
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+
+        // Get Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log('order', order);
+            res.send('Order processed');
+
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Tourism Server is Running');
+});
+
+app.listen(port, () => {
+    console.log('Tourism Server running at port', port);
+})
